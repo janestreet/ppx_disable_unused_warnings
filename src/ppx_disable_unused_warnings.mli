f7d436e709723444afa777b3b1b99cbcece54932
Xavier Clerc <xclerc@janestreet.com>
2020-08-10 09:54:19 +0100

    v0.15-preview.123.10+171

--- src/ppx_disable_unused_warnings.mli
+++ src/ppx_disable_unused_warnings.mli
@@ -1,5 +1,3 @@
 open Ppxlib
 
-val expand_disabled_unused_warnings_attribute
-  :  Parsetree.attribute
-  -> Parsetree.attribute
+val expand_disabled_unused_warnings_attribute : Parsetree.attribute -> Parsetree.attribute
